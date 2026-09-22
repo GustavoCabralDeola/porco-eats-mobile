478cba0dfc48b64451e6f6c1d227793c28be4c38
João Pedro Altini Schmitz <joao65907886@alunos.sc.senac.br>
2026-09-22 20:58:33 -0300

    implemented app_colors

---
+++ lib/shared/widgets/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2D170B);
+  static const Color secondary = Color(0xFFFFB719);
+  static const Color accent = Color(0xFFE31F20);
+  static const Color background = Color(0xFFF7F6F2);
+}
