2a8aafcce4281c61c1a9ce94630cba8b278348e4
João Pedro Altini Schmitz <joao65907886@alunos.sc.senac.br>
2026-09-22 21:25:28 -0300

    implemented text_colors im app_colors

--- lib/shared/widgets/app_colors.dart
+++ lib/shared/widgets/app_colors.dart
@@ -5,4 +5,6 @@ class AppColors {
   static const Color secondary = Color(0xFFFFB719);
   static const Color accent = Color(0xFFE31F20);
   static const Color background = Color(0xFFF7F6F2);
+  static const Color textPrimary = Color(0xFF2D170B);
+  static const Color textSecondary = Color(0xFF777777);
 }
