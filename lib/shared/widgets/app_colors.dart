37590867448a87b5d049e0689e0202edff22a891
João Pedro Altini Schmitz <joao65907886@alunos.sc.senac.br>
2026-09-23 19:06:49 -0300

    changed name of variables of colors

--- lib/shared/widgets/app_colors.dart
+++ lib/shared/widgets/app_colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF2D170B);
-  static const Color secondary = Color(0xFFFFB719);
-  static const Color accent = Color(0xFFE31F20);
-  static const Color background = Color(0xFFF7F6F2);
-  static const Color textPrimary = Color(0xFF2D170B);
-  static const Color textSecondary = Color(0xFF777777);
+  static const Color darkBrown = Color(0xFF2D170B);
+  static const Color yellowAgility = Color(0xFFFFB719);
+  static const Color redDelivery = Color(0xFFE31F20);
+  static const Color brownWhite = Color(0xFFF7F6F2);
+  static const Color title = Color(0xFF2D170B);
+  static const Color subTitle = Color(0xFF777777);
 }
