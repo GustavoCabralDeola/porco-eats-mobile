56258e12289774f5617e05ad0aa550efe2d33db1
Gustavo Deola <gustavodeola@gmail.com>
2026-09-24 18:07:52 -0300

    refactor: composing textformfields for login page

---
+++ lib/shared/widgets/app_text_form_field.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:porco_eats/shared/widgets/app_colors.dart';
+
+class AppTextFormField extends StatefulWidget {
+  const AppTextFormField(
+    this.keyboardType, {
+    super.key,
+    required this.hintText,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.onChanged,
+    this.textEditingcontroller,
+    this.prefixIcon,
+  });
+
+  final String hintText;
+  final bool obscureText;
+  final IconData? prefixIcon;
+  final Widget? suffixIcon;
+  final TextInputType? keyboardType;
+  final Function(String)? onChanged;
+  final TextEditingController? textEditingcontroller;
+
+  @override
+  State<AppTextFormField> createState() => _AppTextFormFieldState();
+}
+
+class _AppTextFormFieldState extends State<AppTextFormField> {
+  late bool isObscure;
+  @override
+  void initState() {
+    isObscure = widget.obscureText;
+    super.initState();
+  }
+
+  void toggleObscure() {
+    setState(() {
+      isObscure = !isObscure;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: widget.keyboardType,
+      autovalidateMode: AutovalidateMode.onUnfocus,
+      decoration: InputDecoration(
+        hintText: widget.hintText,
+        prefixIcon: widget.prefixIcon != null
+            ? Padding(
+                padding: const EdgeInsets.only(left: 24.0, right: 12.0),
+                child: Icon(widget.prefixIcon, color: AppColors.darkBrown),
+              )
+            : null,
+        filled: true,
+        fillColor: AppColors.fullWhite,
+        contentPadding: EdgeInsets.symmetric(vertical: 18),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(28),
+          borderSide: BorderSide(color: AppColors.borderInputColor),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(28),
+          borderSide: BorderSide(color: Colors.black87, width: 1.5),
+        ),
+      ),
+    );
+  }
+}
