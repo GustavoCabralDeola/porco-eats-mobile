1eaf1065e710c7da4abe32b1d2ff733b99f16e4a
Kaua Rodrigo <kr868706@gmail.com>
2026-09-25 03:27:52 -0300

    feat: implement password recovery feature

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:porco_eats/features/login/pages/login_page.dart';
+import 'package:porco_eats/features/recover/pages/recover_page.dart';
 
 class AppRoutes {
   static final Map<String, WidgetBuilder> routes = {
     LoginPage.route: (context) => LoginPage(),
+    RecoverPage.route: (context) => RecoverPage(),
   };
 }
