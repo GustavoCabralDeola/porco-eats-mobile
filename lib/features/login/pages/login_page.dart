79521c8edbd75eb0bdf8c13f704b50fcfac61708
Gustavo Deola <gustavodeola@gmail.com>
2026-09-25 18:11:32 -0300

    feat: adding snackbar for login and adding a login form

--- lib/features/login/pages/login_page.dart
+++ lib/features/login/pages/login_page.dart
@@ -1,11 +1,15 @@
+import 'package:custom_snackbar_plus/custom_snackbar_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:porco_eats/features/home/pages/home_page.dart';
 import 'package:porco_eats/features/login/controllers/login_controller.dart';
 import 'package:porco_eats/shared/widgets/app_checkbox.dart';
 import 'package:porco_eats/shared/widgets/app_colors.dart';
 import 'package:porco_eats/shared/widgets/app_login_header.dart';
 import 'package:porco_eats/shared/widgets/app_text_style.dart';
+import 'package:porco_eats/shared/widgets/exceptions/auth_exception.dart';
 import 'package:provider/provider.dart';
 
+import '../../../shared/widgets/app_elevated_button.dart';
 import '../../../shared/widgets/app_text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
@@ -44,114 +48,104 @@ class LoginPage extends StatelessWidget {
 
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 44),
-                  child: Column(
-                    children: [
-                      AppTextFormField(
-                        TextInputType.emailAddress,
-                        hintText: 'Digite seu email',
-                        prefixIcon: Icons.person_outline,
-                      ),
+                  child: Form(
+                    key: controller.formKey,
+                    child: Column(
+                      children: [
+                        AppTextFormField(
+                          TextInputType.emailAddress,
+                          textEditingcontroller: controller.emailController,
+                          validator: (value) {
+                            return controller.validEmail(value);
+                          },
+                          hintText: 'Digite seu email',
+                          prefixIcon: Icons.person_outline,
+                        ),
 
-                      SizedBox(height: 20),
-
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
+                        SizedBox(height: 20),
+                        AppTextFormField(
+                          textEditingcontroller: controller.passwordController,
+                          validator: (value) {
+                            return controller.validPassword(value);
+                          },
+                          TextInputType.visiblePassword,
                           hintText: 'Digite a sua senha',
-                          prefixIcon: Padding(
-                            padding: EdgeInsets.only(left: 24.0, right: 12.0),
-                            child: Icon(
-                              Icons.lock_outline,
-                              color: AppColors.darkBrown,
-                            ),
-                          ),
-                          suffixIcon: Icon(
-                            Icons.visibility_off_outlined,
-                            color: AppColors.darkBrown,
-                          ),
-                          filled: true,
-                          fillColor: AppColors.fullWhite,
-                          contentPadding: EdgeInsets.symmetric(vertical: 18),
-
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(28),
-                            borderSide: BorderSide(
-                              color: AppColors.borderInputColor,
-                              width: 1.5,
-                            ),
-                          ),
-
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(28),
-                            borderSide: BorderSide(
-                              color: Colors.black87,
-                              width: 1.5,
-                            ),
-                          ),
+                          prefixIcon: Icons.lock_outline,
+                          suffixIcon: Icons.visibility_off_outlined,
+                          obscureText: true,
                         ),
-                      ),
+                        SizedBox(height: 10),
 
-                      SizedBox(height: 10),
-
-                      Row(
-                        children: [
-                          AppCheckBox(
-                            value: controller.isActiveCheckBox,
-                            onChanged: (value) {
-                              controller.changeActiveCheckBox(value!);
-                            },
-                          ),
-                          Text('Lembrar de mim'),
-                          Spacer(),
-                          TextButton(
-                            onPressed: () {
-                              //   Navigator.pushNamed(context, RecoverPage.route);
-                            },
-                            child: Text(
-                              'Esqueci a senha',
-                              style: TextStyle(color: AppColors.redDelivery),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 70),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Navigator.pushNamed(context, SignPage.route);
-                        },
-
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size.fromHeight(48),
-                          foregroundColor: AppColors.fullWhite,
-                          backgroundColor: AppColors.redDelivery,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        Row(
                           children: [
-                            Icon(Icons.arrow_right),
-                            SizedBox(width: 5),
-                            Text('ENTRAR'),
+                            AppCheckBox(
+                              value: controller.isActiveCheckBox,
+                              onChanged: (value) {
+                                controller.changeActiveCheckBox(value!);
+                              },
+                            ),
+                            Text('Lembrar de mim'),
+                            Spacer(),
+                            TextButton(
+                              onPressed: () {
+                                //   Navigator.pushNamed(context, RecoverPage.route);
+                              },
+                              child: Text(
+                                'Esqueci a senha',
+                                style: TextStyle(color: AppColors.redDelivery),
+                              ),
+                            ),
                           ],
                         ),
-                      ),
+                        SizedBox(height: 70),
+                        AppElevatedButton(
+                          prefixIcon: Icons.arrow_forward,
+                          label: 'ENTRAR',
+                          isLoading: controller.isLoading,
+                          onPressed: () async {
+                            try {
+                              final loginSucess = await controller
+                                  .handleLogin();
 
-                      SizedBox(height: 50),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Ainda não tem conta?'),
-                          TextButton(
-                            onPressed: () {
-                              //Navigator.pushNamed(context, RecoverPage.route);
-                            },
-                            child: Text(
-                              'Cadastre-se',
-                              style: TextStyle(color: AppColors.redDelivery),
+                              if (!loginSucess) {
+                                return;
+                              }
+
+                              Navigator.popAndPushNamed(
+                                context,
+                                HomePage.route,
+                              );
+                            } on AuthException catch (e) {
+                              CustomSnackbar.show(
+                                context: context,
+                                title: 'Erro ao fazer login',
+                                label: e.message,
+                                type: SnackbarType.error,
+                                duration: Duration(seconds: 3),
+                              );
+                            }
+                          },
+                          type: ButtonType.filled,
+                        ),
+
+                        SizedBox(height: 50),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Ainda não tem conta?'),
+                            TextButton(
+                              onPressed: () {
+                                //Navigator.pushNamed(context, RecoverPage.route);
+                              },
+                              child: Text(
+                                'Cadastre-se',
+                                style: TextStyle(color: AppColors.redDelivery),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
