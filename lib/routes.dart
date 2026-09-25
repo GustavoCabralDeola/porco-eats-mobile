5b1bd3c5ba5c58c5aa9ff1bd412a4338e9f4bf92
Gustavo Deola <gustavodeola@gmail.com>
2026-09-25 18:13:46 -0300

    feat: adding home page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:porco_eats/features/home/pages/home_page.dart';
 import 'package:porco_eats/features/login/pages/login_page.dart';
 
 class AppRoutes {
   static final Map<String, WidgetBuilder> routes = {
     LoginPage.route: (context) => LoginPage(),
+    HomePage.route: (context) => HomePage(),
   };
 }
